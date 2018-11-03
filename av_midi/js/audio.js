ca948f702ec8e4ba1e8856562ee19a8c626c5bea
Martin <s0553459@htw-berlin.de>
2018-11-03 13:34:32 +0100

    fixed some logic errors + cleanup

--- av_midi/js/audio.js
+++ av_midi/js/audio.js
@@ -1,5 +1,8 @@
+// create Context
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
+let source;
+let stream;
 
 // get the audio element
 const audioElement = document.querySelector('audio');
@@ -9,31 +12,30 @@ const track = audioCtx.createMediaElementSource(audioElement);
 
 track.connect(audioCtx.destination);
 
-
 // select our play button
 const playButton = document.querySelector('button');
 
 playButton.addEventListener('click', function() {
 
-    // check if context is in suspended state (autoplay policy)
-    if (audioCtx.state === 'suspended') {
-        audioCtx.resume();
-    }
+  // check if context is in suspended state (autoplay policy)
+  if (audioCtx.state === 'suspended') {
+    audioCtx.resume();
+  }
 
-    // play or pause track depending on state
-    if (this.dataset.playing === 'false') {
-        audioElement.play();
-        this.dataset.playing = 'true';
-    } else if (this.dataset.playing === 'true') {
-        audioElement.pause();
-        this.dataset.playing = 'false';
-    }
+  // play or pause track depending on state
+  if (this.dataset.playing === 'false') {
+    audioElement.play();
+    this.dataset.playing = 'true';
+  } else if (this.dataset.playing === 'true') {
+    audioElement.pause();
+    this.dataset.playing = 'false';
+  }
 
 }, false);
 
 
 audioElement.addEventListener('ended', () => {
-    playButton.dataset.playing = 'false';
+  playButton.dataset.playing = 'false';
 }, false);
 
 const gainNode = audioCtx.createGain();
@@ -43,5 +45,5 @@ track.connect(gainNode).connect(audioCtx.destination);
 const volumeControl = document.querySelector('#volume');
 
 volumeControl.addEventListener('input', function() {
-    gainNode.gain.value = this.value;
+  gainNode.gain.value = this.value;
 }, false);
