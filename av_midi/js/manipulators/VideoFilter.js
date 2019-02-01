ee52367f8588cbdc8bc20f3459a9cdcdc3a23efe
Butanding <koni.spam@gmail.com>
2019-02-01 13:47:09 +0100

    Nicefied Code, added comments, updated namings, cleaned up unnecessary development-stage codeblocks

--- av_midi/js/manipulators/VideoFilter.js
+++ av_midi/js/manipulators/VideoFilter.js
@@ -1,10 +1,17 @@
 class videoFilter {
 
-
+    /**
+     * Generic filter class that will create a filter according to given-filter function
+     * @param videoEl on which the Canvasian-Filter is to be applied
+     * @param canvasEl that overlays the video
+     * @param filterFunction that will be applied on canvas -> video
+     * @param applyFilter state, a boolean to toggle the activity of the filter
+     */
     constructor(videoEl, canvasEl, filterFunction, applyFilter) {
 
-
-        console.log("Creating ChromaKeying Filter");
+        /**
+         * Initialize all Class-Variables and Parameters
+         */
         this.video = videoEl;
         this.canvas = canvasEl;
         this.ctx = this.canvas.getContext("2d");
@@ -12,17 +19,20 @@ class videoFilter {
         this.applyFilter = applyFilter;
 
         /**
-         * Resize Canvas
+         * Resize Canvas, to make it fit on video flawlessly
+         * Be careful with bootstrap: Canvas-size might not be responsive once initialized
+         * @Todo: Make Canvas Size Responsive working with Bootstrap
          */
         let w = this.video.offsetWidth;
         let h = this.video.offsetHeight;
         let cv = canvasEl;
         cv.width = w;
         cv.height =h;
-
-
     }
 
+    /**
+     * Render Function which will alter the frames and pixels of the video and overlaying canvas
+     */
     render() {
 
         let self = this;
@@ -32,6 +42,7 @@ class videoFilter {
         let frame = this.ctx.getImageData(0, 0, self.video.clientWidth, self.video.clientHeight);
         let length = frame.data.length / 4;
 
+        //Apply Filter only, if wanted/toggled by user
         if (this.applyFilter) {
             this.filter(frame, length);
         }
@@ -40,6 +51,9 @@ class videoFilter {
 
     }
 
+    /**
+     * Disable/Enable filter
+     */
     toggleFilter() {
         let self = this;
 
