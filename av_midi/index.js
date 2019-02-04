5fc8118d62be9761bf357f9532db86d737ea58c2
Butanding <koni.spam@gmail.com>
2019-02-04 18:37:27 +0100

    Fully working Audio-Main Controller Initialized Video-Main Controller

--- av_midi/index.js
+++ av_midi/index.js
@@ -22,6 +22,29 @@ function handleFileSelect(evt) {
     }
 }
 
+function loadRandomAudioSamples(evt){
+    console.log("Loading random audio Samples");
+    let soundSamplesFolder = './res/soundfiles/';
+
+    let soundSamples = [
+        "./res/soundfiles/audience.mp3",
+        "./res/soundfiles/basic_beat.wav",
+        "./res/soundfiles/basic_loop.wav",
+        "./res/soundfiles/classic_music.wav",
+    ]
+
+    for (let i = 0; i < soundSamples.length; i++) {
+        if (emptyAudioSlotCheck()) {
+            let trackNumber = TrackManager.findFirstEmptyAudioTrack();
+            TrackManager.addAudioTrack(trackNumber, soundSamples[i]);
+            TrackManager.getAudioTrack(trackNumber).loadUrlIntoBuffer(soundSamples[i]);
+            addComponentToUI(TrackManager.getAudioTrack(trackNumber));
+        }
+    }
+}
+
+
+
 function emptyAudioSlotCheck() {
     if (TrackManager.findFirstEmptyAudioTrack() < 0) {
         console.info("No free Slot available");
@@ -88,6 +111,74 @@ function addVideoComponentToUI(component) {
     currentEqualizer.appendChild(component);
 }
 
+function pauseAll(){
+    for (let i=0; i<4; i++){
+        if(TrackManager.getAudioTrack(i) != null){
+            if(TrackManager.getAudioTrack(i).isPlaying){
+                TrackManager.getAudioTrack(i).pausePlayback();
+            }
+        }
+    }
+}
+
+function playAll(){
+    for (let i=0; i<4; i++){
+        if(TrackManager.getAudioTrack(i) != null){
+            if(!TrackManager.getAudioTrack(i).isPlaying){
+                TrackManager.getAudioTrack(i).startPlayback();
+            }
+        }
+    }
+}
+
+function muteAll(){
+    for (let i=0; i<4; i++){
+        if(TrackManager.getAudioTrack(i) != null){
+            TrackManager.getAudioTrack(i).changeVolume(0);
+        }
+    }
+}
+
+function unmuteAll(){
+    for (let i=0; i<4; i++){
+        if(TrackManager.getAudioTrack(i) != null){
+            TrackManager.getAudioTrack(i).changeVolume(60);
+        }
+    }
+}
+
+
+function resetAll(){
+    for (let i=0; i<4; i++){
+        if(TrackManager.getAudioTrack(i) != null){
+            TrackManager.getAudioTrack(i).stopPlayback();
+            TrackManager.getAudioTrack(i).shadowRoot.innerHTML = null;
+            TrackManager.deleteAudioTrack(i);
+        }
+    }
+}
+
+
+function loadRandomVideoSamples() {
+
+}
+
+function pauseAllVideos() {
+
+}
+
+function playAllVideos() {
+
+}
+
+function muteAllVideos() {
+
+}
+
+function resetAllVideos() {
+
+}
+
 // Hier startet die App
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -98,5 +189,47 @@ document.addEventListener('DOMContentLoaded', () => {
         let videoUploadButton = document.getElementById('uploadVideoTrack');
         videoUploadButton.addEventListener('change', handleFileSelect, false);
 
+        /**
+         * General Audio-Controller
+         * @type {HTMLElement}
+         */
+        let loadRandomAudio = document.getElementById('loadRandomAudio');
+        loadRandomAudio.addEventListener('click', loadRandomAudioSamples, false);
+
+        let pauseAllChannels = document.getElementById('pauseAllChannels');
+        pauseAllChannels.addEventListener('click', pauseAll, false);
+
+        let playAllChannels = document.getElementById('playAllChannels');
+        playAllChannels.addEventListener('click', playAll, false);
+
+        let muteAllChannels = document.getElementById('muteAllChannels');
+        muteAllChannels.addEventListener('click', muteAll, false);
+
+        let unmuteAllChannels = document.getElementById('unmuteAllChannels');
+        unmuteAllChannels.addEventListener('click', unmuteAll, false);
+
+        let resetAllChannels = document.getElementById('resetAllChannels');
+        resetAllChannels.addEventListener('click', resetAll, false);
+
+    /**
+     * General Video-Controller
+     */
+    let loadRandomVideo = document.getElementById('loadRandomVideo');
+    loadRandomVideo.addEventListener('click', loadRandomVideoSamples, false);
+
+    let pauseAllVideoChannels = document.getElementById('pauseAllVideoChannels');
+    pauseAllVideoChannels.addEventListener('click', pauseAllVideos, false);
+
+    let playAllVideoChannels = document.getElementById('playAllVideoChannels');
+    playAllVideoChannels.addEventListener('click', playAllVideos, false);
+
+    let muteAllVideoChannels = document.getElementById('muteAllVideoChannels');
+    muteAllVideoChannels.addEventListener('click', muteAllVideos, false);
+
+    let unmuteAllVideoChannels = document.getElementById('unmuteAllVideoChannels');
+    unmuteAllVideoChannels.addEventListener('click', unmuteAll, false);
+
+    let resetAllVideoChannels = document.getElementById('resetAllVideoChannels');
+    resetAllVideoChannels.addEventListener('click', resetAllVideos, false);
     }
 );
